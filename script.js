73d20a2b5b2ab09d3d75a2c57d75c2c22cb4292d
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-05 22:13:11 -0500

    Coding From Scratch JS Data Types Activity

---
+++ script.js
@@ -0,0 +1,17 @@
+var myFavoriteNumber = 3;
+var myFavoritePet = "Dog"; 
+var rainIsLovely = true;
+
+var numberOne = 24;
+var numberTwo = 57;
+var addNumbers = numberOne + numberTwo;
+var divideNumbers = numberOne / numberTwo; 
+
+console.log(myFavoriteNumber);
+console.log(myFavoritePet);
+console.log(rainIsLovely);
+
+console.log(numberOne);
+console.log(numberTwo);
+console.log(addNumbers);
+console.log(divideNumbers);
